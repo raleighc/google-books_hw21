9355b162acbc7fd0bd5e4cbd2ce9f24c680e4143
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-11-08 19:04:26 -0500

    all books loading on saved book page.

--- client/src/components/SavedBooks/SavedBooks.jsx
+++ client/src/components/SavedBooks/SavedBooks.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
-import "./SavedBooks.css"
+import "./SavedBooks.css";
 
 const SavedBooks = () => {
+  const [allSavedBooks, setAllSavedBooks] = useState([]);
+
+  useEffect(() => {
+    loadSavedBooks();
+  }, []);
+
+  const loadSavedBooks = () => {
+    Axios.get("/api/books").then((res) => {
+      setAllSavedBooks(res.data);
+    });
+  };
+
+ 
   return (
     <div className="container">
       <div className="row">
@@ -10,37 +24,36 @@ const SavedBooks = () => {
           <div className="card w-100">
             <div className="card-body">
               <h5 className="card-title">Saved Books</h5>
-              <div className="card w-90">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-sm-8 text-left">
-                      <h5>Book Title</h5>
-                      <h6>Book description</h6>
+
+              {allSavedBooks.map((book) => (
+                <div className="card w-90">
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col-sm-8 text-left">
+                        <h5>{book.title}</h5>
+                        <h6>{book.authors}</h6>
+                      </div>
+                      <div className="col-sm-4 text-right">
+                        <button className="btn btn-outline-info">View</button>
+                        <button className="btn btn-outline-info">Delete</button>
+                      </div>
                     </div>
-                    <div className="col-sm-4 text-right">
-                      <button className="btn btn-outline-info">View</button>
-                      <button className="btn btn-outline-info">Delete</button>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-2">
-                      <img
-                        src="https://www.fillmurray.com/200/200"
-                        alt="Placeholder"
-                      />
-                    </div>
-                    <div className="col-sm-10">
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Assumenda asperiores nesciunt rerum placeat enim,
-                        deserunt eaque totam necessitatibus tempora, ullam culpa
-                        voluptates aliquam? Totam, error sequi cupiditate
-                        excepturi explicabo asperiores.
-                      </p>
+                    <div className="row">
+                      <div className="col-sm-2">
+                        <img
+                          src={book.image}
+                          alt="cover of book"
+                        />
+                      </div>
+                      <div className="col-sm-10">
+                        <p>
+                        {book.description}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
